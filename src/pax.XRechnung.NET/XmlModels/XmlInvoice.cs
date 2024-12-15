5a02e61024cc8a4f4bab1e1179a24653310f12cb
ipax77 <ph.ilipp@web.de>
2024-12-15 05:31:57 +0100

    xsd cleanup

--- src/pax.XRechnung.NET/XmlModels/XmlInvoice.cs
+++ src/pax.XRechnung.NET/XmlModels/XmlInvoice.cs
@@ -121,26 +121,3 @@ public class XmlInvoice
     [SpecificationId("BG-25")]
     public List<XmlInvoiceLine> InvoiceLines { get; set; } = [];
 }
-
-/// <summary>
-/// Eine Gruppe von Informationselementen für rechnungsrelevante Erläuterungen mit Hinweisen auf den Rechnungsbetreff.
-/// </summary>
-public class XmlInvoiceNote
-{
-    /// <summary>
-    /// Der Betreff für den nachfolgenden Textvermerk zur Rechnung.
-    /// </summary>
-    [XmlElement("SubjectCode", Namespace = XmlInvoiceWriter.CommonBasicComponents)]
-    [SpecificationId("BT-21")]
-    [CodeList("UNTDID_4451")]
-    public string? SubjectCode { get; set; }
-    /// <summary>
-    /// Ein Textvermerk, der unstrukturierte Informationen enthält, die für die Rechnung als Ganzes maßgeblich sind. 
-    /// Erforderlichenfalls können Angaben zur Aufbewahrungspflicht gem. § 14 Abs. 4 Nr. 9 UStG hier aufgenommen werden.
-    ///   Anmerkung: Im Falle einer bereits fakturierten Rechnung kann hier z. B. der Grund der Korrektur angegeben 
-    ///   werden.
-    /// </summary>
-    [XmlElement("Note", Namespace = XmlInvoiceWriter.CommonBasicComponents)]
-    [SpecificationId("BT-21")]
-    public string Note { get; set; } = string.Empty;
-}
---
+++ src/pax.XRechnung.NET/XmlModels/XmlInvoiceNote.cs
@@ -0,0 +1,28 @@
+﻿using System.Xml;
+using System.Xml.Serialization;
+using pax.XRechnung.NET.Attributes;
+
+namespace pax.XRechnung.NET.XmlModels;
+
+/// <summary>
+/// Eine Gruppe von Informationselementen für rechnungsrelevante Erläuterungen mit Hinweisen auf den Rechnungsbetreff.
+/// </summary>
+public class XmlInvoiceNote
+{
+    /// <summary>
+    /// Der Betreff für den nachfolgenden Textvermerk zur Rechnung.
+    /// </summary>
+    [XmlElement("SubjectCode", Namespace = XmlInvoiceWriter.CommonBasicComponents)]
+    [SpecificationId("BT-21")]
+    [CodeList("UNTDID_4451")]
+    public string? SubjectCode { get; set; }
+    /// <summary>
+    /// Ein Textvermerk, der unstrukturierte Informationen enthält, die für die Rechnung als Ganzes maßgeblich sind. 
+    /// Erforderlichenfalls können Angaben zur Aufbewahrungspflicht gem. § 14 Abs. 4 Nr. 9 UStG hier aufgenommen werden.
+    ///   Anmerkung: Im Falle einer bereits fakturierten Rechnung kann hier z. B. der Grund der Korrektur angegeben 
+    ///   werden.
+    /// </summary>
+    [XmlElement("Note", Namespace = XmlInvoiceWriter.CommonBasicComponents)]
+    [SpecificationId("BT-21")]
+    public string Note { get; set; } = string.Empty;
+}
