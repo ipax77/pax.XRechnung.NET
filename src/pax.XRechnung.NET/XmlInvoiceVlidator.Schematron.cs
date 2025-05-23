b29b450df5ddab2ada987c122fd4fb04c72f09ae
ipax77 <ph.ilipp@web.de>
2025-05-23 09:19:58 +0200

    add http status code to schematron validation

--- src/pax.XRechnung.NET/XmlInvoiceVlidator.Schematron.cs
+++ src/pax.XRechnung.NET/XmlInvoiceVlidator.Schematron.cs
@@ -82,11 +82,12 @@ public static partial class XmlInvoiceValidator
 
         if (!string.IsNullOrEmpty(kositResult.Error) || !kositResult.IsValid)
         {
-            validationEvents.Add(new(new XmlSchemaException("xml invalid"), kositResult.Error ?? "Unexpected error.", XmlSeverityType.Error));
+            validationEvents.Add(new(new XmlSchemaException("unknown"), kositResult.Error ?? kositResult.HttpStatusCode.ToString(), XmlSeverityType.Error));
         }
 
         var result = new InvoiceValidationResult(validationEvents)
         {
+            HttpStatusCode = kositResult.HttpStatusCode.ToString(),
             Evaluation = kositResult.Evaluation,
             Conformity = kositResult.Conformity
         };
